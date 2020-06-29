a6b14953571f1fb13816edd79ea7b62a70e5e051
PANKYAAA <pankajs.gaikar@gmail.com>
2020-06-30 02:23:14 +0530

    Created a ViewLayout for list screen.

--- MobileProject-MVVM/ViewController/RootViewController.swift
+++ MobileProject-MVVM/ViewController/RootViewController.swift
@@ -7,13 +7,65 @@
 //
 
 import UIKit
+import SnapKit
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, ConstraintRelatableTarget {
 
+    private var tableView: UITableView!
+    private var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
+        
+        // With this property, we set which sides of your view can be extended to cover the whole screen.
+        // When the view of that view controller is laid out, it will start where the navigation bar ends
+        self.edgesForExtendedLayout = []
+        
+        //Setup tableview
+        setupTableView()
+        
+        //Setup segmented control
+        setupSegmentedControl()
+        
+        //Setup Constraints
+        layoutSubViews()
     }
+    
+    //MARK: Setup Views
+    fileprivate func setupSegmentedControl() -> Void {
+        segmentedControl = UISegmentedControl(items: ["All", "Image", "Text", "Other"] )
+        self.view.addSubview(segmentedControl)
+        segmentedControl.addTarget(self, action: #selector(segmentedValueChanged(_:)), for: .valueChanged)
+    }
+    
+    fileprivate func setupTableView() -> Void {
+        tableView = UITableView()
+        self.view.addSubview(tableView)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100.0
+    }
+    
+    /*
+     * This API creates a View Layout for Data List screen. 
+     */
+    fileprivate func layoutSubViews() -> Void {
+        segmentedControl.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+
+        tableView.snp.makeConstraints { (make) in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(segmentedControl.snp.bottom)
+        }
+    }
+    
+    //MARK: Segmented Control Actions
+    @objc func segmentedValueChanged(_ sender:UISegmentedControl!) {
+        print("Selected Segment Index is : \(sender.selectedSegmentIndex)")
+    }
+
 
 }
 
