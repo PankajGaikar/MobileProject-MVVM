2edba25ba6cea0a942d1b63e939ddfc381db8b1b
PANKYAAA <pankajs.gaikar@gmail.com>
2020-06-30 00:39:39 +0530

    Added couple of more groups for extensions, API service and Utilities. Added Constants file.

---
+++ MobileProject-MVVM/APIService/APIService.swift
@@ -0,0 +1,13 @@
+//
+//  APIService.swift
+//  MobileProject-MVVM
+//
+//  Created by Pankaj Gaikar on 30/06/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import Foundation
+
+class APIservice {
+    
+}
---
+++ MobileProject-MVVM/Utilities/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  MobileProject-MVVM
+//
+//  Created by Pankaj Gaikar on 30/06/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import Foundation
