b0b60a3a61593c571c33644dc9ce128bd69963fe
PANKYAAA <pankajs.gaikar@gmail.com>
2020-06-30 00:37:25 +0530

    Added project structure. Added groups for Model, ViewModel and ViewControllers. Added initial classes. Deleted Storyboard file and removed it's reference from project.

---
+++ MobileProject-MVVM/Model/DataModel.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  MobileProject-MVVM
+//
+//  Created by Pankaj Gaikar on 30/06/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import Foundation
+
+struct DataModel {
+    
+}
---
+++ MobileProject-MVVM/View/DataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataTableViewCell.swift
+//  MobileProject-MVVM
+//
+//  Created by Pankaj Gaikar on 30/06/20.
+//  Copyright © 2020 Pankaj Gaikar. All rights reserved.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
